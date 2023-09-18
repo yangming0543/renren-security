4fd77359c20b33757adfb120d447a143fdf00519
Mark <sunlightcs@gmail.com>
2023-09-18 10:59:10 +0800

    优化代码

--- renren-admin/src/main/java/io/renren/modules/oss/cloud/CloudStorageConfig.java
+++ renren-admin/src/main/java/io/renren/modules/oss/cloud/CloudStorageConfig.java
@@ -1,14 +1,16 @@
 /**
  * Copyright (c) 2018 人人开源 All rights reserved.
- *
+ * <p>
  * https://www.renren.io
- *
+ * <p>
  * 版权所有，侵权必究！
  */
 
 package io.renren.modules.oss.cloud;
 
-import io.renren.common.validator.group.*;
+import io.renren.common.validator.group.AliyunGroup;
+import io.renren.common.validator.group.QcloudGroup;
+import io.renren.common.validator.group.QiniuGroup;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -29,12 +31,12 @@ import java.io.Serializable;
 public class CloudStorageConfig implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty(value = "类型 1：七牛  2：阿里云  3：腾讯云   4：FastDFS   5：本地上传   6：MinIO")
-    @Range(min=1, max=6, message = "{oss.type.range}")
+    @ApiModelProperty(value = "类型 1：七牛  2：阿里云  3：腾讯云 ")
+    @Range(min = 1, max = 6, message = "{oss.type.range}")
     private Integer type;
 
     @ApiModelProperty(value = "七牛绑定的域名")
-    @NotBlank(message="{qiniu.domain.require}", groups = QiniuGroup.class)
+    @NotBlank(message = "{qiniu.domain.require}", groups = QiniuGroup.class)
     @URL(message = "{qiniu.domain.url}", groups = QiniuGroup.class)
     private String qiniuDomain;
 
@@ -42,19 +44,19 @@ public class CloudStorageConfig implements Serializable {
     private String qiniuPrefix;
 
     @ApiModelProperty(value = "七牛ACCESS_KEY")
-    @NotBlank(message="{qiniu.accesskey.require}", groups = QiniuGroup.class)
+    @NotBlank(message = "{qiniu.accesskey.require}", groups = QiniuGroup.class)
     private String qiniuAccessKey;
 
     @ApiModelProperty(value = "七牛SECRET_KEY")
-    @NotBlank(message="{qiniu.secretkey.require}", groups = QiniuGroup.class)
+    @NotBlank(message = "{qiniu.secretkey.require}", groups = QiniuGroup.class)
     private String qiniuSecretKey;
 
     @ApiModelProperty(value = "七牛存储空间名")
-    @NotBlank(message="{qiniu.bucketname.require}", groups = QiniuGroup.class)
+    @NotBlank(message = "{qiniu.bucketname.require}", groups = QiniuGroup.class)
     private String qiniuBucketName;
 
     @ApiModelProperty(value = "阿里云绑定的域名")
-    @NotBlank(message="{aliyun.domain.require}", groups = AliyunGroup.class)
+    @NotBlank(message = "{aliyun.domain.require}", groups = AliyunGroup.class)
     @URL(message = "{aliyun.domain.url}", groups = AliyunGroup.class)
     private String aliyunDomain;
 
@@ -62,23 +64,23 @@ public class CloudStorageConfig implements Serializable {
     private String aliyunPrefix;
 
     @ApiModelProperty(value = "阿里云EndPoint")
-    @NotBlank(message="{aliyun.endPoint.require}", groups = AliyunGroup.class)
+    @NotBlank(message = "{aliyun.endPoint.require}", groups = AliyunGroup.class)
     private String aliyunEndPoint;
 
     @ApiModelProperty(value = "阿里云AccessKeyId")
-    @NotBlank(message="{aliyun.accesskeyid.require}", groups = AliyunGroup.class)
+    @NotBlank(message = "{aliyun.accesskeyid.require}", groups = AliyunGroup.class)
     private String aliyunAccessKeyId;
 
     @ApiModelProperty(value = "阿里云AccessKeySecret")
-    @NotBlank(message="{aliyun.accesskeysecret.require}", groups = AliyunGroup.class)
+    @NotBlank(message = "{aliyun.accesskeysecret.require}", groups = AliyunGroup.class)
     private String aliyunAccessKeySecret;
 
     @ApiModelProperty(value = "阿里云BucketName")
-    @NotBlank(message="{aliyun.bucketname.require}", groups = AliyunGroup.class)
+    @NotBlank(message = "{aliyun.bucketname.require}", groups = AliyunGroup.class)
     private String aliyunBucketName;
 
     @ApiModelProperty(value = "腾讯云绑定的域名")
-    @NotBlank(message="{qcloud.domain.require}", groups = QcloudGroup.class)
+    @NotBlank(message = "{qcloud.domain.require}", groups = QcloudGroup.class)
     @URL(message = "{qcloud.domain.url}", groups = QcloudGroup.class)
     private String qcloudDomain;
 
@@ -86,23 +88,23 @@ public class CloudStorageConfig implements Serializable {
     private String qcloudPrefix;
 
     @ApiModelProperty(value = "腾讯云AppId")
-    @NotNull(message="{qcloud.appid.require}", groups = QcloudGroup.class)
+    @NotNull(message = "{qcloud.appid.require}", groups = QcloudGroup.class)
     private Integer qcloudAppId;
 
     @ApiModelProperty(value = "腾讯云SecretId")
-    @NotBlank(message="{qcloud.secretId.require}", groups = QcloudGroup.class)
+    @NotBlank(message = "{qcloud.secretId.require}", groups = QcloudGroup.class)
     private String qcloudSecretId;
 
     @ApiModelProperty(value = "腾讯云SecretKey")
-    @NotBlank(message="{qcloud.secretkey.require}", groups = QcloudGroup.class)
+    @NotBlank(message = "{qcloud.secretkey.require}", groups = QcloudGroup.class)
     private String qcloudSecretKey;
 
     @ApiModelProperty(value = "腾讯云BucketName")
-    @NotBlank(message="{qcloud.bucketname.require}", groups = QcloudGroup.class)
+    @NotBlank(message = "{qcloud.bucketname.require}", groups = QcloudGroup.class)
     private String qcloudBucketName;
 
     @ApiModelProperty(value = "腾讯云COS所属地区")
-    @NotBlank(message="{qcloud.region.require}", groups = QcloudGroup.class)
+    @NotBlank(message = "{qcloud.region.require}", groups = QcloudGroup.class)
     private String qcloudRegion;
 
 }
